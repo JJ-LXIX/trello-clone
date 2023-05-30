ef412a90ac674deeabfc2d2bc4a8923e5579a0eb
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-05-30 12:03:57 +0530

    Header Component created and added to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
     <main>
+      <Header />
+
+      {/* TODO BOARD */}
       <h1>Hello World!</h1>
     </main>
   );
