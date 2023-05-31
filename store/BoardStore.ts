2b61d23725c731296924b0c5ae7c35520999b2d0
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-05-31 21:32:37 +0530

    added zustand BoardStore

---
+++ store/BoardStore.ts
@@ -0,0 +1,24 @@
+import { databases } from "@/appwrite";
+import { getTodosGroupedByColumn } from "@/lib/getTodosGroupedByColumn";
+import { create } from "zustand";
+
+interface BoardState {
+  board: Board;
+  getBoard: () => void;
+  setBoardState: (board: Board) => void;
+  updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void;
+}
+
+export const userBoardStore = create<BoardState>((set) => ({
+  board: {
+    columns: new Map<TypedColumn, Column>(),
+  },
+
+  getBoard: async () => {
+    const board = await getTodosGroupedByColumn();
+    set({ board });
+  },
+
+  setBoardState: (board) => set({ board }),
+
+}));
